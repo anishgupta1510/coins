ba9d6d6a244a02bf2134ffe848ef87c6d204a05e
anish <anishgupta700@gmail.com>
2022-12-13 15:38:09 +0530

    footer and loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,15 @@
 import { Box, Spinner, VStack } from '@chakra-ui/react'
 import React from 'react'
+import {FillingBottle} from 'react-cssfx-loading'
 
 function Loader() {
   return (
     <>
       <VStack h={"90vh"} justifyContent={"center"}>
 
-        <Box transform={"scale(20)"}>
-            <Spinner size={"x1"}/>
+        <Box transform={"scale(1)"}>
+            {/* <Spinner size={"x1"}/> */}
+            <FillingBottle color='black' duration='4s' />
         </Box>
 
       </VStack>
